0f45711789fd2594239e967572f4ee316cc32cbc
obaraelijah <elijahobara357@gmail.com>
2023-01-22 06:38:13 +0000

    Hot fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,29 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers
- * @array: The array to be sorted
- * @size: Size of the array
- * Return: Void
- */
-
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t counter1, counter2;
-	int temp_swap, flag = 0;
+	size_t i = 0, j = 0;
+	int aux = 0;
 
-	/*Check inputs*/
 	if (array == NULL || size == 0)
-	{
 		return;
-	}
-
-	/* Counts through the current step in the sort */
-	for (counter1 = 0; counter1 < size - 1; counter1++)
+	for (; i < size - 1; i++)
 	{
-		/* Loop through array */
-		for (counter2 = 0; counter2 < size - counter1 - 1; counter2++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[counter2] > array[counter2 + 1])
+			if (array[j] > array[j + 1])
 			{
-				/* Swap a[i] with a[1 + 1] */
-				temp_swap = array[counter2];
-				array[counter2] = array[counter2 + 1];
-				array[counter2 + 1] = temp_swap;
-				/* Print result */
+				aux = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = aux;
 				print_array(array, size);
-				flag = 1;
 			}
-		}
-		if (flag == 0)
-		{
-			break;
 		}
 	}
 }
