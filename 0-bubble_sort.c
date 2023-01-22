e8dd84d87a725354187c3d3a0b433433cb553b45
obaraelijah <elijahobara357@gmail.com>
2023-01-22 06:30:23 +0000

    Hot fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,43 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - Sorts an array of integers
+ * @array: The array to be sorted
+ * @size: Size of the array
+ * Return: Void
  */
+
 void bubble_sort(int *array, size_t size)
 {
+	size_t counter1, counter2;
+	int temp_swap, flag = 0;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	/*Check inputs*/
+	if (array == NULL || size == 0)
+	{
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	}
+
+	/* Counts through the current step in the sort */
+	for (counter1 = 0; counter1 < size - 1; counter1++)
+	{
+		/* Loop through array */
+		for (counter2 = 0; counter2 < size - counter1 - 1; counter2++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[counter2] > array[counter2 + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				/* Swap a[i] with a[1 + 1] */
+				temp_swap = array[counter2];
+				array[counter2] = array[counter2 + 1];
+				array[counter2 + 1] = temp_swap;
+				/* Print result */
+				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+		{
+			break;
+		}
+	}
 }
